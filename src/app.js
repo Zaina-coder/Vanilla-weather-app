1b842f646b8f1c153bbe2d9db450cbc96520b0a5
Zaina-coder <81202142+Zaina-coder@users.noreply.github.com>
2021-04-04 13:19:06 -0700

    work on current temp

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function displayweather(response) {
+   let temperatureElement = document.querySelector("#temperature");
+   let cityElement= document.querySelector("#city");
+   let descriptionElement=document.querySelector("#description");
+   let humidityElement=document.querySelector("#humidity");
+   let windElement=document.querySelector("#wind")
+   temperatureElement.innerHTML= Math.round(response.data.main.temp);
+   cityElement.innerHTML=response.data.name; 
+   descriptionElement.innerHTML=response.data.weather[0].description;
+   humidityElement.innerHTML=response.data.main.humidity;
+   windElement.innerHTML=Math.round(response.data.wind.speed);
+}
+
+
+let apiKey ="dbe2579a231e00f951c0c9e165264e61";
+ let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+ console.log(apiUrl);
+ axios.get(apiUrl).then(displayweather);
+
+ 
